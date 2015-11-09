ca433ed8bf7043889f86062d1aa3eb74f39749f0
firmansp <firmanblues42@gmail.com>
2015-11-09 11:27:00 +0700

    Initial commit

---
+++ app/src/main/java/com/exclusive/firmansp/crud/domain/Siswa.java
@@ -0,0 +1,35 @@
+package com.exclusive.firmansp.crud.domain;
+
+/**
+ * Created by firmansp on 09/11/15.
+ */
+public class Siswa  {
+
+    private String id;
+    private String nama;
+    private String kelas;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+}
